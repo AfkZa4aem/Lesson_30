bf0f3f27b72a46921e0b0c50a105de90bb748064
AfkZa4aem <nastypinagrably@gmail.com>
2017-01-16 14:41:52 +0300

    add more /visit validation

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ require 'sinatra/activerecord'
 set :database,  "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
-	validates :phone, presence: true
+	validates :name, presence: true, length: { minimum: 2 }
+	validates :phone, presence: true, numericality: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
 end
@@ -17,7 +17,7 @@ class Barber < ActiveRecord::Base
 end
 
 class Contact < ActiveRecord::Base
-	validates :email, presence: true
+	validates :email, presence: true # , message: 'Where is your email! Bitch!'
 	validates :message, presence: true
 end
 
